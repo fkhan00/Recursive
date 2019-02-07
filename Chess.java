24bd40270059b9499cdc11eec5278d599886e110
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-02-06 19:19:22 -0500

    Add files via upload

--- Chess.java
+++ Chess.java
@@ -10,7 +10,13 @@ public class Chess{
     for(int i = 0; c - i >= 0 && r - i >= 0; i++){
       if(board[r - i][c - i] == -1){
         return false;}}
-    return true;}
+    for(int i = 0; c + i <= board.length && r - i >= 0; i++){
+        if(board[r - i][c  + i] == -1){
+          return false;}}
+    for(int i = 0; r + i <= board.length && c - i >= 0; i++){
+          if(board[r + i][c - i] == -1){
+             return false;}}
+    return board[r][c] == 0;}
 
   public static void edit(int r, int c, boolean add){
     int output = -1;
@@ -26,20 +32,23 @@ public class Chess{
     for(int i = 0; c - i >= 0 && r - i >= 0; i++){
       if(board[r - i][c - i] != -1){
       board[r-i][c-i] += output;}}
-    board[r][c] = 0;}
-  public static int count(int num){
-    int counter = 0;
-    for(int i = 0; i < board.length; i++){
-      for(int j = 0; j < board.length; j++){
-        if(board[i][j] == -1){
-          counter ++;}}}
-    return counter;}
+    for(int i = 0; r + i <= board.length && c - i >= 0; i++){
+      if(board[r + i][c - i] != -1){
+        board[r + i][c - i] += output;}}
+    for(int i = 0; c + i <= board.length && r - i >= 0; i++){
+      if(board[r - i][c + i] != -1){
+        board[r - i][c + i] += output;}}
+    board[r][c] = 0;
+    if(add){
+      board[r][c] = -1;}
+    }
 
   public static boolean nQueensH(int r, int c){
     int startOff = 0;
-    if(count(-1) == board.length - 1){
+    System.out.println(printArray());
+    if(c == board.length - 1 && valid(r, c)){
       return true;}
-    if(c == board.length){
+    if(r == board.length && c == board.length){
       return false;}
     if(r == board.length){
       while(startOff < board.length && board[startOff][c - 1] != -1){
@@ -48,13 +57,24 @@ public class Chess{
       return nQueensH(startOff, c - 1);}
     if(valid(r, c)){
       edit(r, c, true);
-      return nQueensH(r, c  + 1);}
+      return nQueensH(0, c  + 1);}
     return nQueensH(r + 1, c);}
   public static boolean nQueens(int n){
     board = new int[n][n];
     return nQueensH(0, 0);}
+  public static String printArray(){
+    String output = "";
+    for(int i = 0; i < board.length; i++){
+      for(int j = 0; j < board.length; j++){
+        output += " " + board[i][j];}
+      output += "\n";}
+    return output;}
   public static void main(String[] args){
-    System.out.println(nQueens(8));
-    System.out.println(board);
+    try{
+    System.out.println(nQueens(4));
+    System.out.println(printArray());
+  }
+  catch(StackOverflowError e){
+    System.out.println("");}
   }
 }
