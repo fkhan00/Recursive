b5b34b2a968bdab8ec9ceb76db2565851c1a7b24
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-02-07 04:51:34 -0500

    Add files via upload

--- Chess.java
+++ Chess.java
@@ -1,23 +1,15 @@
 public class Chess{
   static int[][] board;
   public static boolean valid(int r, int c){
-    for(int i = 0; i < board.length; i++){
-      if(board[i][c] == -1 && board[r][i] == -1){
-        return false;}}
-    for(int i = 0; c + i < board.length && r + i < board.length; i++){
-      if(board[i + r][i + c] == -1){
-        return false;}}
-    for(int i = 0; c - i >= 0 && r - i >= 0; i++){
-      if(board[r - i][c - i] == -1){
-        return false;}}
-    for(int i = 0; c + i <= board.length && r - i >= 0; i++){
-        if(board[r - i][c  + i] == -1){
-          return false;}}
-    for(int i = 0; r + i <= board.length && c - i >= 0; i++){
-          if(board[r + i][c - i] == -1){
-             return false;}}
     return board[r][c] == 0;}
-
+  public static int count(int num){
+    int counter = 0;
+    for(int i  = 0; i < board.length; i++){
+      for(int j = 0; j < board.length; j++){
+        if(board[i][j] == num){
+          counter ++;}}}
+    return counter;
+  }
   public static void edit(int r, int c, boolean add){
     int output = -1;
     if(add){
@@ -32,10 +24,10 @@ public class Chess{
     for(int i = 0; c - i >= 0 && r - i >= 0; i++){
       if(board[r - i][c - i] != -1){
       board[r-i][c-i] += output;}}
-    for(int i = 0; r + i <= board.length && c - i >= 0; i++){
+    for(int i = 0; r + i < board.length && c - i >= 0; i++){
       if(board[r + i][c - i] != -1){
         board[r + i][c - i] += output;}}
-    for(int i = 0; c + i <= board.length && r - i >= 0; i++){
+    for(int i = 0; c + i < board.length && r - i >= 0; i++){
       if(board[r - i][c + i] != -1){
         board[r - i][c + i] += output;}}
     board[r][c] = 0;
@@ -46,15 +38,15 @@ public class Chess{
   public static boolean nQueensH(int r, int c){
     int startOff = 0;
     System.out.println(printArray());
-    if(c == board.length - 1 && valid(r, c)){
+    if(count(-1) == board.length){
       return true;}
-    if(r == board.length && c == board.length){
+    if(r >= board.length && c >= board.length){
       return false;}
-    if(r == board.length){
+    if(r >= board.length){
       while(startOff < board.length && board[startOff][c - 1] != -1){
         startOff ++;}
       edit(startOff, c - 1, false);
-      return nQueensH(startOff, c - 1);}
+      return nQueensH(startOff + 1, c - 1);}
     if(valid(r, c)){
       edit(r, c, true);
       return nQueensH(0, c  + 1);}
@@ -71,7 +63,7 @@ public class Chess{
     return output;}
   public static void main(String[] args){
     try{
-    System.out.println(nQueens(4));
+    System.out.println(nQueens(8));
     System.out.println(printArray());
   }
   catch(StackOverflowError e){
